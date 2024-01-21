4c043cf68f4e3d57b7021af0bd4ce3c9ab13181d
KeitaAoto <KeitaAotoMail@gmail.com>
2024-01-21 09:26:35 +0900

    Pipes now spawn randomly

--- FlappyBirdGitHubTest/Assets/PipeSpawnerScript.cs
+++ FlappyBirdGitHubTest/Assets/PipeSpawnerScript.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PipeSpawnerScript : MonoBehaviour
 {
     private float timer = 0f;
     [SerializeField] private float spawnRate = 2.5f;
+    [SerializeField] private float random = 5;
 
     [SerializeField] private GameObject pipes;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(pipes, transform.localPosition, Quaternion.identity);   
+        Instantiate(pipes, new Vector3(transform.localPosition.x, Random.Range(-random, random), 0), Quaternion.identity);   
     }
 
     // Update is called once per frame
@@ -21,7 +23,7 @@ public class PipeSpawnerScript : MonoBehaviour
         timer += Time.deltaTime;
         if (timer >= spawnRate)
         {
-            Instantiate(pipes, transform.localPosition, Quaternion.identity);
+            Instantiate(pipes, new Vector3(transform.localPosition.x, Random.Range(-random, random), 0), Quaternion.identity);
             timer = 0f;
         }
     }
